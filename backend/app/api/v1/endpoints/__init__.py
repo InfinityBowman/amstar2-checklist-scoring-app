44fb183b4c5c3fc76e71521b1a47ba7415aa3b8e
Jacob Maynard <jacobamaynard@proton.me>
2025-10-17 13:15:37 -0500

    remove electric routes from fastapi since they dont even work

--- backend/app/api/v1/endpoints/__init__.py
+++ backend/app/api/v1/endpoints/__init__.py
@@ -1,6 +1,5 @@
 from .auth import router as auth_router
 from .users import router as users_router
-from .electric import router as electric_router
 from .projects import router as projects_router
 from .project_members import router as project_members_router
 from .reviews import router as reviews_router
@@ -11,7 +10,6 @@ from .checklist_answers import router as checklist_answers_router
 __all__ = [
     "auth_router", 
     "users_router", 
-    "electric_router",
     "projects_router",
     "project_members_router",
     "reviews_router",
